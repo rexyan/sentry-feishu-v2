f2d31cd46777ba90210983f8a6e5ef4db7c71307
runsha.yan <runsha.yan@cyclone-robotics.com>
2022-03-03 19:50:23 +0800

    up

--- src/sentry_feishu/plugin.py
+++ src/sentry_feishu/plugin.py
@@ -95,7 +95,7 @@ class FeiShuPlugin(NotificationPlugin):
                             {
                                 "is_short": True,
                                 "text": {
-                                    "content": u"**📍 环境信息**\n " + event.get_tag('environment') or "",
+                                    "content": u"**📍 环境信息**\n " + "",
                                     "tag": "lark_md"
                                 }
                             },
